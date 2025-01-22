e912c41e507497577161f284b996c9c3dc97f704
mutsukake <overgrown333@gmail.com>
2025-01-22 22:51:58 +0900

    Add script to print a chessboard based on user-defined height and width

---
+++ print_a_chessboard.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for {
+		var h, w int
+		fmt.Scanln(&h, &w)
+		if h == 0 && w == 0 {
+			break
+		}
+
+		for i := 0; i < h; i++ {
+			for j := 0; j < w; j++ {
+				if (i+j)%2 == 0 {
+					fmt.Print("#")
+				} else {
+					fmt.Print(".")
+				}
+			}
+			fmt.Println()
+		}
+		fmt.Println()
+	}
+}
